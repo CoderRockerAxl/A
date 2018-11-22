fa0a5b1fc378f67262bdcd12b5930fb969c31f2f
sunguan <sun_guan@suixingpay.com>
2018-11-22 11:52:02 +0800

    6

---
+++ A/A/BViewController.h
@@ -0,0 +1,17 @@
+//
+//  BViewController.h
+//  Mediator
+//
+//  Created by zwy on 2018/11/21.
+//  Copyright © 2018年 suixingpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A/A/BViewController.m
@@ -0,0 +1,32 @@
+//
+//  BViewController.m
+//  Mediator
+//
+//  Created by zwy on 2018/11/21.
+//  Copyright © 2018年 suixingpay. All rights reserved.
+//
+
+#import "BViewController.h"
+
+@interface BViewController ()
+
+@end
+
+@implementation BViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.orangeColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
